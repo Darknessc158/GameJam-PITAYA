e13ac750bccd720f102c38dd9a7e4c6021e71c3d
Momiouo <47118225+Momiouo@users.noreply.github.com>
2020-02-03 10:33:35 +0100

    test

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import pygame
 
+pygame.init()
+screen = pygame.display.set_mode((1024, 768))
 
-print("Groupe PITAYA")
+launched = True
 
-test
+while launched:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            launched = False
