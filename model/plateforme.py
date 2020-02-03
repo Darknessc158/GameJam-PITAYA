d3fbcf3b150ff8cd12af76cc13dd5fad81d45736
Ng-B <naodg68@gmail.com>
2020-02-03 16:44:26 +0100

    g

--- model/plateforme.py
+++ model/plateforme.py
@@ -1,10 +1,28 @@
 class Plateforme:
-    def __init__(self, name, damage):
-        self.name = name
-        self.damage = damage
+    def __init__(self, x, y, isDangerous):
+        self.x = x
+        self.y = y
+        self.isDangerous=isDangerous
 
-    def get_name(self):
-        return self.name
+    def get_x(self):
+        return self.x
 
+    def get_y(self):
+        return self.y
+
+    def get_isDangerous(self):
+        return self.isDangeous
+
+    def set_x(self, x):
+        self.x=x
+
+    def set_y(self, y):
+        self.y=y
+
+    def set_isDangerous(self, isDangerous):
+        self.isDangeous=isDangerous
+
+    """ 
     def get_damage_amount(self):
         return self.damage
+    """
