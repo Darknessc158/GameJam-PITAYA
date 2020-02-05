ba4a0fe9ad17439c8f927c32b84c4211058c0569
Momiouo <47118225+Momiouo@users.noreply.github.com>
2020-02-05 12:50:39 +0100

    gazbottle

--- Model/game.py
+++ Model/game.py
@@ -2,6 +2,9 @@ import sys
 sys.path.append('../GameJam-PITAYA/View')
 import pygame
 from pygame.locals import *
+from Model.objet import Objet
+from Model.objet import Carburant
+import random
 
 
 class Game:
@@ -12,6 +15,9 @@ class Game:
 
     def get_score(self):
         return self.score
+
+    def add_score(self, score):
+        self.score += score
 
     def get_time(self):
         return self.time
@@ -35,3 +41,30 @@ class Game:
 
             pygame.display.update()
             mainClock.tick(60)
+
+    def generate_objet(self):
+        i = 0
+        x = 0
+        y = 0
+        powerups = []
+        while i < 5: # 1ere 5 carburants
+            i += 1
+            x = int(random.random()*1000)
+            y = int(-random.random()*1000)
+            carb = Carburant("carburant", x, y, 50)
+            powerups.insert(i, carb)
+
+        i = 0
+        while i < 3: # 2 gazbottle
+            i += 1
+            x = int(random.random() * 1000)
+            y = int(-random.random() * 1000)
+            objet = Objet("bouteille", x, y)
+            powerups.insert(i, objet)
+
+
+        return powerups
+
+
+if __name__ == '__main__':
+    print(int(-random.random()*1000))
